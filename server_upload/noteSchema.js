f9d647cd84b5f5af20c9f820d998f39c869eb320
Yevhenii Shypitsyn <yevhenii.shypitsyn@gmail.com>
2022-03-20 17:04:51 -0400

    Final version of app and server code for now
    To run the app you need to include NODE MODULES!!! In a while, I'll add user login page to the app and needed code for users in the database. Also, I will host the code on server.

---
+++ server_upload/noteSchema.js
@@ -0,0 +1,15 @@
+var mongoose = require("mongoose")
+var Schema = mongoose.Schema
+
+var note = new Schema({
+
+    title: String,
+    date: String,
+    note: String,
+    folder: String
+
+})
+
+const Data = mongoose.model("Data", note)
+//
+module.exports = Data
